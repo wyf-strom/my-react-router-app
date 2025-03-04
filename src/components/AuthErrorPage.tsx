f7660ce6f7ec1e5500b2816ffbb5377bc155fee6
wangyifeng <wangyifeng@email.com>
2025-03-04 16:20:44 +0800

    test: :white_check_mark: 测试提交工具

--- src/components/AuthErrorPage.tsx
+++ src/components/AuthErrorPage.tsx
@@ -1,12 +1,13 @@
 /*
  * @Date: 2025-03-03 14:54:25
  * @LastEditors: wangyifeng
- * @LastEditTime: 2025-03-04 16:04:18
+ * @LastEditTime: 2025-03-04 16:18:08
  * @Description: 无权限页面
  */
 export default function AuthErrorPage() {
   return (
     <div className='layout'>
+      <h1>暂无权限访问，请联系管理员</h1>
       <h1>暂无权限访问，请联系管理员</h1>
       <h1>暂无权限访问，请联系管理员</h1>
       <h1>暂无权限访问，请联系管理员</h1>
