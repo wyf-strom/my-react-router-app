784a0a7f2cd9f9e0f2b07634fbe200c87d01ab79
王翌峰 <wangyifeng@wangyifengdeMac-mini.local>
2025-03-05 12:58:20 +0800

    feat: :sparkles: 新增

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,12 +1,13 @@
 /*
  * @Date: 2025-03-03 14:55:16
  * @LastEditors: wangyifeng
- * @LastEditTime: 2025-03-05 12:40:07
+ * @LastEditTime: 2025-03-05 12:54:40
  * @Description:
  */
 export default function Dashboard() {
   return (
     <div className='layout'>
+      <h1>正常访问</h1>
       <h1>正常访问</h1>
     </div>
   );
